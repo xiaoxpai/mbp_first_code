0afe984ad89b1750947c83cf5a34a32212c01237
mbp_xiaolh@163.com <mbp_xiaolh@163.com>
2022-06-18 23:20:15 +0800

    first init repository

---
+++ mbp-struts2-spring-init/src/main/java/io/mbp/action/LoginAction.java
@@ -0,0 +1,15 @@
+package io.mbp.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class LoginAction implements Action {
+    public static void main(String[] args){
+
+
+    }
+
+    public String execute() throws Exception {
+
+        return SUCCESS;
+    }
+}
