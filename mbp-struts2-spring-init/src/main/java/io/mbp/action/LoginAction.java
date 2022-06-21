de1e036dfb7843017c3b6d9b11b33f7e24ece982
mbp_xiaolh@163.com <mbp_xiaolh@163.com>
2022-06-22 00:41:16 +0800

    @doc add commit in LoginAction.

--- mbp-struts2-spring-init/src/main/java/io/mbp/action/LoginAction.java
+++ mbp-struts2-spring-init/src/main/java/io/mbp/action/LoginAction.java
@@ -2,6 +2,10 @@ package io.mbp.action;
 
 import com.opensymphony.xwork2.Action;
 
+/**
+ * @updateTime 2022年06月22日 ALF
+ * @deprecated 登陆功能模块
+ */
 public class LoginAction implements Action {
     public static void main(String[] args){
 
