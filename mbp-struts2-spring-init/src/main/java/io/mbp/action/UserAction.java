b4e511e82f4f89026bce9caf88cd3a17df670ec5
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-07-06 10:51:34 +0800

    create UserAction
    add action

---
+++ mbp-struts2-spring-init/src/main/java/io/mbp/action/UserAction.java
@@ -0,0 +1,86 @@
+package actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.interceptor.ParameterNameAware;
+import models.MessageStore;
+import org.apache.struts2.interceptor.SessionAware;
+
+import java.util.Map;
+
+public class UserAction extends ActionSupport implements SessionAware, ParameterNameAware {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * The model class that stores the message
+     * to display in the view.
+     */
+    private MessageStore messageStore;
+
+    private Map<String, Object> userSession;
+
+    private static final String HELLO_COUNT = "helloCount";
+
+    private String userName;
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+
+    public void setMessageStore(MessageStore messageStore) {
+        this.messageStore = messageStore;
+    }
+
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String queryUser() {
+        messageStore = new MessageStore();
+        //Action included a query string parameter of userName
+        //or a form field with name of userName
+        if (userName != null) {
+            messageStore.setMessage(messageStore.getMessage() + " " + userName);
+
+        }
+        increaseHelloCount();
+        return SUCCESS;
+    }
+
+    /**
+     *@TODO 增加存储在用户的HELLO COUNT的值。
+     */
+    private void increaseHelloCount() {
+        Integer helloCount = (Integer) userSession.get(HELLO_COUNT);
+
+        if (helloCount == null) {
+            helloCount = 1;
+        } else {
+            helloCount++;
+        }
+        userSession.put(HELLO_COUNT, helloCount);
+    }
+
+
+    public boolean acceptableParameterName(String parameterName) {
+        boolean allowedParameterName = true ;
+
+        if ( parameterName.contains("session")  || parameterName.contains("request") ) {
+
+            allowedParameterName = false ;
+
+        }
+        return allowedParameterName;
+    }
+
+    public void setSession(Map<String, Object> session) {
+        userSession = session ;
+    }
+}
