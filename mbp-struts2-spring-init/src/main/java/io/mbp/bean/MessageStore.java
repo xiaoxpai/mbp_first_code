96b5f9e8b1bf17e47a1fc88815534cb78ccc9e24
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-07-11 11:00:43 +0800

    Create MessageStore.java
    Create MessageStore.java

---
+++ mbp-struts2-spring-init/src/main/java/io/mbp/bean/MessageStore.java
@@ -0,0 +1,25 @@
+
+public class MessageStore {
+
+    private String message;
+
+    public MessageStore() {
+
+        setMessage("Hello Struts User");
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String toString() {
+
+        return message + " (from toString)";
+
+    }
+
+}
