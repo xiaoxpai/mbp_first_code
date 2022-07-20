e622bf1f7d77dc53602b729f6b0c2ab7ca63cae7
小小沠 <39144603+xiaoxpai@users.noreply.github.com>
2022-07-20 21:01:50 +0800

    Update Ebook.js
    Update Ebook.js

--- mbp-struts2-spring-init/src/main/webapp/ebook/app/store/EBook.js
+++ mbp-struts2-spring-init/src/main/webapp/ebook/app/store/EBook.js
@@ -1,1 +1,23 @@
 //数据处理
+ 
+Ext.define('FormSet.store.Form', {
+			extend : 'Ext.data.Store',
+			autoDestroy : true,
+			//storeId : 'formStore',
+			pageSize : 10,
+			proxy : {
+				type : 'ajax', 
+				noCache : false, 
+				actionMethods : {
+					read : 'POST'
+				},
+				url : contextPath+'/ebook/bookAction!getJsonListBook.action',
+				reader : {
+					type : 'json',
+					root : 'data',
+					idProperty : 'bookId'
+				}
+			},
+			model : 'ebook.model.Book'
+
+		});
